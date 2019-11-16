4c1cd4ff753df513bc7aa4c4e65d6e692b2de973
tao <enzocxt@gmail.com>
2019-11-16 16:45:00 +0100

    add: header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- HomeHeader 对应 home-header -->
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont menu-icon">&#xe6ec;</div>
+    </div>
+    <div class="header-input">
+      <div class="iconfont search-icon">&#xe70a;</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  /* 样式中引入需要用 ~@import */
+  @import '~@styles/variables.styl'
+  /* 1rem = html font-size = 50px */
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor /* #00bcd4 */
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .menu-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      margin-bottom: .12rem
+      margin-right: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+      .search-icon
+        padding-right: .4rem
+        float: right
+</style>
